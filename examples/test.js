e5344a38a265a8d3c9a36d4aab92a60d6f5358e7
lxla1989 <lxla1989@163.com>
2018-07-18 10:22:19 +0800

    update
    修复未过滤变量名或者值为undefined或null的bug

--- examples/test.js
+++ examples/test.js
@@ -1,4 +1,6 @@
 const parseQstring = require('../lib/parseQstring');
-let qs = 'at>15,tid==01,at<200,hid~=A04,at!=101,hid!=A01,pid==,cid!=00,undefined==1';
+let qs = 'at>15,tid==,tid>=,tid<=,at<200,hid~=A04,at!=101,hid!=A01,pid==,cid!=00,undefined==1,at==Undefined';
+// let qs = ',tid==,tid>=,at>15,,tid<=,tid~=5,';
+// let qs = 'td==Undefined';
 let tt = parseQstring(qs);
-console.log(JSON.stringify(tt)); //{"tid":{"$eq":"008098022c9b"},"at":{"$gt":1508717995100,"$lt":1508724704042,"$ne":200},"hid":{"$regex":"A01122330003","$ne":"B01122330003"}}
+console.log(JSON.stringify(tt));
