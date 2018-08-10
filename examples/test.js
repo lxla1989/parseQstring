3e0fc27d45f616d58b6214f8c78d3cf0ab55cfc7
lxla1989 <lxla1989@163.com>
2018-08-10 16:18:28 +0800

    update
    1.  加入大小不敏感
    2.  加入对字符型数字转数值型, 可选
    3.  针对模糊查找时对value作转义字符处理
    4.  用正则替换嵌套for循环

--- examples/test.js
+++ examples/test.js
@@ -1,6 +1,7 @@
 const parseQstring = require('../lib/parseQstring');
-let qs = 'at>15,tid==,tid>=,tid<=,at<200,hid~=A04,at!=101,hid!=A01,pid==,cid!=00,undefined==1,at==Undefined';
+let qs = 'at>=123456<=789,at>123456789,tid==00895485acb,tid>=null,at<233456789,hid~=\A01**1223++300\\03+++,at!=200,hid!=B01122330003,pid==,cid!=00,ppd>,value==138,,mail==abc_1554+da==ddkl@163.com';
 // let qs = ',tid==,tid>=,at>15,,tid<=,tid~=5,';
 // let qs = 'td==Undefined';
+// let qs ='module==,type==,created>=,created<=';
 let tt = parseQstring(qs);
 console.log(JSON.stringify(tt));
